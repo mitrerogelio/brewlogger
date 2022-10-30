f2bbe630d70ee0c43ec26f68dfa38a7672862401
Rogelio Mitre <mitrerogelio@outlook.com>
2022-10-29 23:17:21 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './components/Nav'
+
+const App = () => {
+  return (
+    <>
+        <Nav />
+        <h2>Hola Mundo</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav className='bg-red-500'>
+        <h1 className='bg-blue-500'>Brewlogger</h1>
+    </nav>
+  )
+}
+
+export default Nav
