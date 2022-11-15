a686714659918062ddd2ade99b340e03f94a65a1
Rogelio Mitre <mitrerogelio@outlook.com>
2022-11-14 20:42:41 -0600

    toggle icon on child pages

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,13 @@
-import Nav from '../components/Nav'
+import ChildNav from '../components/ChildNav'
 import Footer from '../components/Footer'
 
 export const Profile = () => {
 
   return (
-    <div>
-      <Nav />
+    <section>
+      <ChildNav />
       <h1 className='text-center text-5xl font-bold w-3/5 mx-auto mt-20'>Edit Profile!</h1>
       <Footer />
-    </div>
+    </section>
   );
 }
