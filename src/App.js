a85d1639d2b3ee99eecabc8a9733221e10d120c9
Rogelio Mitre <mitrerogelio@outlook.com>
2022-12-05 20:46:45 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import { Home } from './pages/Home';
 import { Brew } from './pages/Brew'
 import { Settings } from './pages/Settings'
 import { Profile } from './pages/Profile'
+import { BrewerProvider } from './context/BrewerProvider';
 
 export const App = () => {
   
   return (
     <main className="App">
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/brew/:brewer' element={<Brew />} />
-        <Route path='/settings' element={<Settings />} />
-        <Route path='/profile' element={<Profile />} />
-      </Routes>
+      <BrewerProvider>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/brew/:brewer' element={<Brew />} />
+          <Route path='/settings' element={<Settings />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </BrewerProvider>
     </main>
   )
 }
---
+++ src/context/BrewerProvider.js
@@ -0,0 +1,32 @@
+import { createContext, useState, useEffect } from 'react'
+import {db} from '../firebase/firebase-config'
+import { collection, getDocs } from '@firebase/firestore'
+
+// Create Context
+export const BrewerContext = createContext()
+
+export const BrewerProvider = props => {
+
+    // Create State Obj
+    const [devices, setDevices] = useState([])
+
+    // Fetch Brewers from Firebase
+    const brewersCollectionReference = collection(db, 'brewers')
+    useEffect(() => {
+        const getBrewers = async () => {
+        const data = await getDocs(brewersCollectionReference)
+        setDevices(data.docs.map((doc) => ({...doc.data(), id: doc.id })))
+        }
+        getBrewers()
+
+        console.log('Component Rendered')
+    }, [] )
+
+    console.log(devices)
+
+    return (
+        <BrewerContext.Provider value={[devices, setDevices]}>
+            {props.children}
+        </BrewerContext.Provider>
+    )
+}
