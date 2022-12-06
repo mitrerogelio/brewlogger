1ed76b03be5d183b4952b288cb34e3f96a411843
Rogelio Mitre <mitrerogelio@outlook.com>
2022-12-06 14:51:13 -0600

    clear data inside brew, clear slate

--- src/pages/Brew.js
+++ src/pages/Brew.js
@@ -4,14 +4,7 @@ import { useLocation } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
 export const Brew = () => {
-	// Retrieve brewer obj from <Brewmethods />
-	const {
-		state: { data },
-	} = useLocation()
-
-	// Obj to state
-	const [brewer, setBrewer] = useState({ ...data })
-	// console.log(brewer)
+	// Define Current Brewer
 
 	// Dropdown Options
 	const ratios = [
@@ -31,36 +24,35 @@ export const Brew = () => {
 		'1:20',
 	]
 
-	useEffect(() => {
+	/* useEffect(() => {
 		console.log(`The multiple is now ${brewer.multiple}`)
-	}, [brewer])
+	}, [brewer]) */
 
-	const onOptionChangeHandler = event => {
+	/* const onOptionChangeHandler = event => {
 		const multiple = parseInt(event.target.value.substring(2), 10)
 		const newBrew = { ...brewer, multiple }
 		setBrewer(newBrew)
-	}
+	} */
 
 	// Slider Logic
-	const sliderEventHandler = event => {
+	/* const sliderEventHandler = event => {
 		const dose = parseInt(event.target.value.substring(0), 10)
 		const newBrew = { ...brewer, dose }
 		setBrewer(newBrew)
-	}
+	} */
 
-	const { img, name, description, dose, multiple } = brewer
 	return (
 		<ChildPage>
 			<main className='flex flex-col justify-center align-center p-12'>
 				<article className='card card-compact w-96 bg-neutral shadow-xl justify-center p-12'>
 					<div className='avatar flex justify-center m-10 w-3/4'>
-						<Description info={[name, img, description]} />
+						{/* <Description info={[name, img, description]} /> */}
 					</div>
 
 					{/* Brew Form */}
 					<select
 						className='select select-primary w-full max-w-xs'
-						onChange={onOptionChangeHandler}
+						// onChange={onOptionChangeHandler}
 					>
 						<option>Brew Ratio:</option>
 						{ratios.map((ratio, index) => {
@@ -73,24 +65,24 @@ export const Brew = () => {
 							htmlFor='coffee-dose'
 							className='self-start mb-2'
 						>
-							Coffee (grams): {dose}
+							Coffee (grams): 30g
 						</label>
 						<input
 							type='range'
 							id='coffee-dose'
 							min='10'
 							max='50'
-							defaultValue={dose}
+							defaultValue='30'
 							className='range'
 							step='5'
-							onChange={sliderEventHandler}
+							// onChange={sliderEventHandler}
 						/>
 					</section>
 					<section className='flex flex-col items-center  my-5'>
 						<div className='w-full'>
 							<p>
 								Water Amount:{' '}
-								<span className='text-primary'>{dose * multiple}</span>
+								<span className='text-primary'>0</span>
 								(mL/grams)
 							</p>
 						</div>
