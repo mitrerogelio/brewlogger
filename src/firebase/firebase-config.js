c80549bbf44af66e3b190facb91f32e25dcee04c
Rogelio Mitre <mitrerogelio@outlook.com>
2022-12-26 21:10:34 -0600

    rename js to jsx, configure vite (#49)

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,23 +4,23 @@ import '@firebase/firestore'
 import { getFirestore } from '@firebase/firestore'
 
 const {
-    REACT_APP_FIRE_BASE_KEY,
-    REACT_APP_AUTH_DOMAIN,
-    REACT_APP_PROJECT_ID,
-    REACT_APP_STORAGE_BUCKET,
-    REACT_APP_MESSAGING_SENDER_ID,
-    REACT_APP_APP_ID,
-    REACT_APP_MEASUREMENT_ID
-} = process.env
+    VITE_FIRE_BASE_KEY,
+    VITE_AUTH_DOMAIN,
+    VITE_PROJECT_ID,
+    VITE_STORAGE_BUCKET,
+    VITE_MESSAGING_SENDER_ID,
+    VITE_APP_ID,
+    VITE_MEASUREMENT_ID
+} = import.meta.env
 
 const config = {
-    apiKey: `${REACT_APP_FIRE_BASE_KEY}`,
-    authDomain: `${REACT_APP_AUTH_DOMAIN}`,
-    projectId: `${REACT_APP_PROJECT_ID}`,
-    storageBucket: `${REACT_APP_STORAGE_BUCKET}`,
-    messagingSenderId: `${REACT_APP_MESSAGING_SENDER_ID}`,
-    appId: `${REACT_APP_APP_ID}`,
-    measurementId: `${REACT_APP_MEASUREMENT_ID}`,
+    apiKey: `${VITE_FIRE_BASE_KEY}`,
+    authDomain: `${VITE_AUTH_DOMAIN}`,
+    projectId: `${VITE_PROJECT_ID}`,
+    storageBucket: `${VITE_STORAGE_BUCKET}`,
+    messagingSenderId: `${VITE_MESSAGING_SENDER_ID}`,
+    appId: `${VITE_APP_ID}`,
+    measurementId: `${VITE_MEASUREMENT_ID}`,
 }
 
 const app = initializeApp(config)
